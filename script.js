90754349097e5cca219a6b8c27dbebb2e6fbebe3
shadikmahamudsohan <shadikmahamudsohan@gmail.com>
2022-02-17 13:18:01 +0600

    adding error case for income and expenses input

--- script.js
+++ script.js
@@ -15,11 +15,39 @@ function totalExpensesCount() {
 document.getElementById('calculate-btn').addEventListener('click', function () {
     const totalEspensesAmount = totalExpensesCount();
     const totalIncomeInput = document.getElementById('income-input');
-    const totalBalanceAmount = parseFloat(totalIncomeInput.value);
+    const totalIncomeAmount = parseFloat(totalIncomeInput.value);
 
-    // updating total espenses and balance vlue
-    const totalEspenses = document.getElementById('total-expenses');
-    const totalBalance = document.getElementById('total-balance');
-    totalEspenses.innerText = totalEspensesAmount;
-    totalBalance.innerText = totalBalanceAmount - totalEspensesAmount;
+
+
+    // errors for not number and if the field is empthy
+    const incomeErrorString = document.getElementById('inocme-input-error-1');
+    const incomeErrorNegative = document.getElementById('inocme-input-error-2');
+    const expensesErros = document.getElementsByClassName('expenses-error');
+
+    if (isNaN(totalIncomeAmount)) {
+        incomeErrorString.classList.remove('d-none');
+        incomeErrorNegative.classList.add('d-none');
+    }
+    else if (totalIncomeAmount < 0) {
+        incomeErrorNegative.classList.remove('d-none')
+        incomeErrorString.classList.add('d-none')
+    } else if (isNaN(totalEspensesAmount) || totalEspensesAmount < 0) {
+
+        for (const error of expensesErros) {
+            error.classList.remove('d-none')
+        }
+    } else {
+        // updating total espenses and balance vlue
+        const totalEspenses = document.getElementById('total-expenses');
+        const totalBalance = document.getElementById('total-balance');
+        totalEspenses.innerText = totalEspensesAmount;
+        totalBalance.innerText = totalIncomeAmount - totalEspensesAmount;
+
+        // removing errors
+        incomeErrorString.classList.add('d-none')
+        incomeErrorNegative.classList.add('d-none')
+        for (const error of expensesErros) {
+            error.classList.add('d-none')
+        }
+    }
 })
